6f4b7b9af518dc47e5f3f4bd8cb79fd29657ff38
Samuele Cornell <cornellsamuele@gmail.com>
2025-09-10 09:20:12 +0000

    made easier to modify paths in main.py

--- main.py
+++ main.py
@@ -2,30 +2,37 @@
 from pathlib import Path
 from engine import run_experiment
 
+
+
+SASSEC_PATH ="/disks/disk1/scornell/mapss/MAPSS-measures/SASSEC"  #full path to SASSEC root folder, do not use relative path
+
+
 MANIFEST = [
     {
         "mixture_id": "female_pair_example",
-        "references": [
-            Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\orig\female_inst_sim_1.wav"),
-            Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\orig\female_inst_sim_2.wav"),
+        "references": [ Path(SASSEC_PATH) /
+            Path("Signals/orig/female_inst_sim_1.wav"),  Path(SASSEC_PATH) /
+            Path("Signals/orig/female_inst_sim_2.wav"),
         ],
         "systems": {
-            "Algo1_SASSEC": [
-                Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\Algo1_SASSEC\female_inst_sim_1.wav"),
-                Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\Algo1_SASSEC\female_inst_sim_2.wav"),
+            "Algo1_SASSEC": [  Path(SASSEC_PATH) /
+                Path("Signals/Algo1/female_inst_sim_1.wav"),  Path(SASSEC_PATH) /
+                Path("Signals/Algo1/female_inst_sim_2.wav"),
             ],
         }
     },
     {
         "mixture_id": "male_pair_example",
-        "references": [
-            Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\orig\male_inst_sim_1.wav"),
-            Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\orig\male_inst_sim_2.wav"),
+        "references": [  Path(SASSEC_PATH) /
+            Path("Signals/orig/male_inst_sim_1.wav"), 
+             Path(SASSEC_PATH) /
+            Path("Signals/orig/male_inst_sim_2.wav"),
         ],
         "systems": {
-            "Algo1_SASSEC": [
-                Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\Algo1_SASSEC\male_inst_sim_1.wav"),
-                Path(r"C:\postdoc\R1 - PS and PM\Data - speech separation MOS\Unified\Signals\Algo1_SASSEC\male_inst_sim_2.wav"),
+            "Algo1_SASSEC": [ Path(SASSEC_PATH) /
+                Path("Signals/Algo1/male_inst_sim_1.wav"),
+                 Path(SASSEC_PATH) /
+                Path("Signals/Algo1/male_inst_sim_2.wav"),
             ],
         }
     },
